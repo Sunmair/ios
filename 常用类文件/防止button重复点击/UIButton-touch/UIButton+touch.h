586416c723c7219003fd188814d413276eb84c80
maxiaozheng <eric.ma@toncentsoft.com>
2018-03-27 16:21:04 +0800

    UIButton+touch  只响应第一次。 UIButton+delay  只响应最后一次。

--- 常用类文件/防止button重复点击/UIButton-touch/UIButton+touch.h
+++ 常用类文件/防止button重复点击/UIButton-touch/UIButton+touch.h
@@ -11,7 +11,8 @@
 @interface UIButton (touch)
 /**设置点击时间间隔*/
 @property (nonatomic, assign) NSTimeInterval timeInterval;
+/**
+ *  用于设置单个按钮不需要被hook
+ */
+@property (nonatomic, assign) BOOL isIgnore;
 @end
-// 版权属于原作者
-// http://code4app.com (cn) http://code4app.net (en)
-// 发布代码于最专业的源码分享网站: Code4App.com
